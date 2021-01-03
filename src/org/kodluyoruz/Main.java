9b8b8cbf87510dfd0a04a275c9a3bd8fd1566780
sahindogukan <53344451+sahindogukan@users.noreply.github.com>
2021-01-03 23:44:16 +0300

    Update Main.java

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -3,6 +3,29 @@ package org.kodluyoruz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Queue kuyruk = new Queue();
+
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                kuyruk.kuyrugaGir();
+            }
+        });
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                kuyruk.kuyruktanCık();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        try {
+            producer.join();
+            consumer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
